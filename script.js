f28d85301698df73f5791ff4d7609fe19fba4c59
Mathumika.k <151771307+Mathumika@users.noreply.github.com>
2024-03-15 14:47:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 let http = new XMLHttpRequest();
-http.open('GET', 'products.json', true);
+http.open('get', 'products.json', true);
 http.send();
-
 http.onload = function() {
     if (this.readyState == 4 && this.status == 200) {
         let products = JSON.parse(this.responseText);
         let output = "";
         for (let item of products) {
-            output += `<div class="flex flex-col lg:flex-row items-center justify-evenly">
-                          <div class="flex flex-col items-center justify-center">
-                            <img class="h-72 w-72" src="${item.image}" alt="${item.title}" />
-                            <div class="">${item.title}</div>
-                            <div class="">${item.price}</div>
-                            <a href="${item.addToCartLink}" class="cursor-pointer custom-bg h-6 w-40 text-center text-white text-sm font-semibold items-center justify-center rounded-2xl mx-auto my-2">
-                              Add To Cart
-                            </a>
-                          </div>
-                        </div>`;
+            output += `<div class="product">
+            <img src="${item.image}" alt="${item.image}">
+            <p class="title">${item.title}</p>
+            <p class="description">${item.description}</p>
+            <p class="price">${item.price}</p>
+            <p class="cart" >Buy Now <i class="bx bx-cart-alt"></i></p>
+            </div>`;
         }
         document.querySelector(".products").innerHTML = output;
     }
 }
-
